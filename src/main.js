849d1072f21238fe5c633ecc3f78ad27abd9e87a
Murat Alkan <muratalkanw@hotmail.com>
2022-03-13 19:54:34 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,38 +1,41 @@
+"use strict";
+
 const express = require('express')
-const app = express()
-const RateLimit = require('express-rate-limit');
 const PORT = 3001
 const ENDPOINT = '/api/v1'
 
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//Routers
-const device_router = require('./Routes/device_router')
+const RateLimit = require('express-rate-limit');
 
-
-
-/*
 const cors = require("cors")
 const corsOptions = {
   origin: `http://localhost:${PORT}`
 };
 app.use(cors(corsOptions));
-//app.use(cors())
-*/
 
 const apiLimiter = RateLimit({
 	windowMs: 10 * 60 * 1000, // 10 minutes
 	max: 100, // Limit each IP to 100 requests
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "Too many requests!",
 })
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 app.use(apiLimiter); // apply rate limiter to all requests
+
+
+//Routers
+const device_router = require('./Routes/deviceRouter')
+
+
+app.use((request, response, next) => {
+  next();
+});
+
 app.use(ENDPOINT, device_router);
+
 
 
 
