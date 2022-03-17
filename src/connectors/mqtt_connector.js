17ded2818fd1bef763c26d37df5b7493be23c399
Murat Alkan <muratalkanw@hotmail.com>
2022-03-17 23:11:42 +0000

    Update mqtt connector

--- src/connectors/mqtt_connector.js
+++ src/connectors/mqtt_connector.js
@@ -4,7 +4,7 @@ const mqtt = require('mqtt');
 
 const host = '127.0.0.1'
 const port = '1883'
-const clientID = `mqtt_${Math.random().toString(16).slice(3)}`
+const clientID = `mqtt_${Math.random().toString(16).slice(3)}` // ID=IoTwin_TBGateway
 const connectUrl = `mqtt://${host}:${port}`
 
 module.exports = mqtt.connect(connectUrl, {
