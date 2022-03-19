82d85d81bec59974c31bf133ed3c9bdf5dfa8a76
Murat Alkan <muratalkanw@hotmail.com>
2022-03-19 13:43:31 +0300

    Add new db queries

--- src/controllers/alertController.js
+++ src/controllers/alertController.js
@@ -9,11 +9,25 @@ const tenantID = 1;
 const buildingID = 1;
 
 
+const getAllAlerts = (req, res) => {
+  //////////dbHelper.getAlert(...);
+  pool.query(
+    `SELECT * FROM device_alerts`, 
+    (error, result) => {
+
+    if (error) 
+    {
+      return res.status(400).json(error);
+    }
+
+    return res.status(201).json(result.rows);
+  })
+};
+
 //exports.getAlert = function(req, res) {}
 const getAlert = (req, res) => {
   const id = req.params.id
 
-  console.log(id)
   //////////dbHelper.getAlert(...);
   pool.query(
     `SELECT * FROM device_alerts WHERE sn=$1`, 
@@ -22,10 +36,10 @@ const getAlert = (req, res) => {
 
     if (error) 
     {
-      return res.status(404).json(error);
+      return res.status(400).json(error);
     }
 
-    return res.status(200).json(result.rows);
+    return res.status(201).json(result.rows);
   })
 };
 
@@ -33,10 +47,31 @@ const create_alert= (telemetry) => {
     const telemetry_obj = JSON.parse(telemetry)
     
   };
+
+
+const deleteAlert = (req, res) => {
+  const id = req.params.id
+
+  pool.query(
+    'DELETE FROM device_alerts WHERE id=$1', 
+    [id], 
+    (error, result) => {
+
+    if (error) 
+    {
+      return res.status(400).json(error);
+    }
+
+    return res.status(201).json(result.rows);
+  })
+};
+  
   
 
 
 
 module.exports = {
+  getAllAlerts,
   getAlert,
+  deleteAlert,
 };
