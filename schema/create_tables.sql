8fb43ff686d9488e0d5cdc10735640bf5ee0bdbe
Murat Alkan <muratalkanw@hotmail.com>
2022-04-17 21:12:17 +0000

    Update sql script

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -13,6 +13,7 @@ DROP TABLE IF EXISTS public."tenants";
 CREATE TABLE IF NOT EXISTS public."tenants"
 (
     id UUID DEFAULT gen_random_uuid(),
+    
     name STRING NOT NULL,
     country STRING(50) NOT NULL,
     city STRING(50) NOT NULL,
@@ -20,12 +21,16 @@ CREATE TABLE IF NOT EXISTS public."tenants"
     address STRING(50) NOT NULL,
     phone STRING(50) NOT NULL,
     email STRING(50) NOT NULL,
-    coordinates STRING,
-    description STRING,
+    coordinates STRING(255),
+    description TEXT,
     owner STRING(30) NOT NULL DEFAULT 'admin',
-    timestampz TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
-    CONSTRAINT "primary" PRIMARY KEY (id ASC)
+
+    PRIMARY KEY (id),
+    UNIQUE(name)
 );
 
 INSERT INTO tenants (name, country, city, postcode, address, phone, email) VALUES
@@ -36,116 +41,109 @@ DROP TABLE IF EXISTS public."assets";
 CREATE TABLE IF NOT EXISTS public."assets"
 (
     id UUID DEFAULT gen_random_uuid(),
-    tenant_id INT8 NOT NULL DEFAULT 1,
+    
     name STRING NOT NULL,
     city STRING(50) NOT NULL,
     location STRING(255) NOT NULL,
-    coordinates STRING,
-    description STRING,
+    coordinates STRING(255),
+    description TEXT,
     owner STRING(30) NOT NULL DEFAULT 'admin',
-    timestampz TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    tenant_id STRING(255) NOT NULL DEFAULT 'CTIS',
+
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
-    CONSTRAINT "primary" PRIMARY KEY (id ASC)
+
+    PRIMARY KEY (id)
 );
+-------FOREIGN KEY
 
 ----------- devices
---CREATE SEQUENCE IF NOT EXISTS device_seq;
 DROP TABLE IF EXISTS public."devices";
 CREATE TABLE IF NOT EXISTS public."devices"
 (
     id UUID NOT NULL DEFAULT gen_random_uuid(),
     sn STRING NOT NULL,
+    
     name STRING NOT NULL,
-    protocol STRING(30) NOT NULL DEFAULT 'MQTT',
+    protocol STRING(30) NOT NULL DEFAULT 'mqtt',
     model STRING NOT NULL,
-    type STRING[] NOT NULL,
-    max_values INT[] NOT NULL DEFAULT '[]',
-    description STRING NOT NULL,
+    types STRING[] NOT NULL,
+    max_values INT[] NOT NULL,
+    description TEXT NOT NULL,
     owner STRING(30) NOT NULL DEFAULT 'admin',
-    access_token INT8,
+    access_token INT,
     status BOOL DEFAULT true,
 
-    tenant_id INT NOT NULL DEFAULT 1,
-    asset_id INT NOT NULL DEFAULT 1,
-
-    timestamptz timestamptz DEFAULT NOW(),
-    created_at timestamp DEFAULT NOW(),
-    updated_at TIME,
+    asset_id UUID,
+    tenant_id STRING(255) NOT NULL DEFAULT 'CTIS',
     
-    CONSTRAINT "primary" PRIMARY KEY (id ASC),
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    created_at timestamp DEFAULT NOW(),
+    
+    PRIMARY KEY (id),
     UNIQUE (sn)
 );
+-------FOREIGN KEY
 
 
 ----------- device_telemetries
-DROP TABLE IF EXISTS public."device_telemetries2";
-CREATE TABLE IF NOT EXISTS public."device_telemetries2"
-(
-    id INT DEFAULT unique_rowid(),
-    device_id INT NOT NULL,
-    sn STRING NOT NULL,
-    value JSONB NOT NULL,
-
-    timestamp INT NOT NULL,
-    created_at timestamp DEFAULT NOW(),
-
-    CONSTRAINT "primary" PRIMARY KEY (id ASC)
-);
-
-
 DROP TABLE IF EXISTS public."device_telemetries";
-
-DROP SEQUENCE IF EXISTS telemetry_seq;
-CREATE SEQUENCE telemetry_seq START 1 MINVALUE 0 INCREMENT 1;
-
 CREATE TABLE IF NOT EXISTS public."device_telemetries"
 (
-    id INT PRIMARY KEY DEFAULT nextval('telemetry_seq'),
+    id INT DEFAULT unique_rowid(),
     sn STRING NOT NULL,
     value JSONB NOT NULL,
 
+    device_id UUID,
+    asset_id UUID,
+    tenant_id STRING,
+
     timestamp INT NOT NULL,
-    created_at timestamp DEFAULT NOW()
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
+    created_at timestamp DEFAULT NOW(),
+    
+    PRIMARY KEY (id)
 );
 
+
 ----------- device_alerts
-
-
-
 DROP TABLE IF EXISTS public."device_alerts";
-
-DROP SEQUENCE IF EXISTS alert_seq;
-CREATE SEQUENCE alert_seq START 1 MINVALUE 0 INCREMENT 1;
-
 CREATE TABLE IF NOT EXISTS public."device_alerts"
 (
-    id INT PRIMARY KEY DEFAULT nextval('alert_seq'),
+    id INT DEFAULT unique_rowid(),
     sn STRING NOT NULL,
 
     telemetry_key STRING(50) NOT NULL,
     type STRING(30) NOT NULL,
     message TEXT NOT NULL,
-    status BOOL NOT NULL DEFAULT FALSE,
+    status BOOL NOT NULL DEFAULT false,
+
+    device_id UUID,
+    asset_id UUID,
+    tenant_id STRING,
     
     timestamp INT NOT NULL,
-    created_at timestamp DEFAULT NOW()
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
+    created_at timestamp DEFAULT NOW(),
+
+    PRIMARY KEY (id)
 );
 
----------------id INT DEFAULT unique_rowid(),
----------------CONSTRAINT "primary" PRIMARY KEY (id ASC)
 
 ------------------------------------------------------ LOGS ------------------------------------------------------------------------
-
-
 DROP TABLE IF EXISTS public."system_logs";
 CREATE TABLE IF NOT EXISTS public."system_logs"
 (
     id INT DEFAULT unique_rowid(),
     log_msg TEXT NOT NULL,
-    timestampz TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
 
-    CONSTRAINT "primary" PRIMARY KEY (id ASC)
+    PRIMARY KEY (id)
 );
 
 
@@ -154,10 +152,13 @@ CREATE TABLE IF NOT EXISTS public."tenant_logs"
 (
     id INT DEFAULT unique_rowid(),
     log_msg TEXT NOT NULL,
-    timestampz TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    tenant_id UUID NOT NULL,
+    
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
 
-    CONSTRAINT "primary" PRIMARY KEY (id ASC)
+    PRIMARY KEY (id)
 );
 
 
@@ -166,19 +167,25 @@ CREATE TABLE IF NOT EXISTS public."asset_logs"
 (
     id INT DEFAULT unique_rowid(),
     log_msg TEXT NOT NULL,
-    timestampz TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+    asset_id UUID NOT NULL,
+
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
 
-    CONSTRAINT "primary" PRIMARY KEY (id ASC)
+    PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS public."device_logs";
 CREATE TABLE IF NOT EXISTS public."device_logs"
 (
     id INT DEFAULT unique_rowid(),
-    log TEXT NOT NULL,
-    timestampz TIMESTAMPTZ NOT NULL DEFAULT now(),
+    log_msg TEXT NOT NULL,
+
+    device_id UUID NOT NULL,
+
+    timestamptz TIMESTAMPTZ NOT NULL DEFAULT now(),
     created_at TIMESTAMP NOT NULL DEFAULT now(),
 
-    CONSTRAINT "primary" PRIMARY KEY (id ASC)
+    PRIMARY KEY (id)
 );
