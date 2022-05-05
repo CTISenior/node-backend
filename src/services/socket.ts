9414be55e3342804b4f8799672898ae5314a8b5c
Murat Alkan <muratalkanw@hotmail.com>
2022-05-05 11:01:54 +0300

    Update socket.ts

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,5 @@
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
-
 import kafka from '../connectors/kafka_connector';
 
 const httpServer = createServer();
@@ -13,58 +12,70 @@ const io = new Server(httpServer, {
 });
 
 
-const consumer = {}
+let consumer
 let topic
 
 io.on('connection', (socket) => {
   console.log(`socket_id: ${socket.id}`)
 
   socket.on('telemetry_topic', (arg) => {
-    topic = arg.toString();
+    if(arg){
+      topic = arg.toString();
+    }
 
     if (topic) {
       console.log(`kafka_topic: ${topic}`);
 
-      (async () => {
-        consumer[socket.id] = kafka.consumer({ groupId: `consumer-group_${socket.id}` });
-        await consumer[socket.id].connect()
-        await consumer[socket.id].subscribe({ topic, fromBeginning: false });
-      })();
+      consumer = kafka.consumer({ groupId: `consumer-group_${socket.id}` });
+      consumer.connect()
+      consumer.subscribe({ topic, fromBeginning: false });
 
-      setTimeout(function() {
+      setTimeout(function () {
         consume(topic, socket).catch(console.error);
       }, 1000);
     }
+
+
 
   });
 
   socket.on("disconnect", () => {
     console.info(`Client disconnected [socket_id=${socket.id}]`);
-    if(topic){
+    if (topic) {
       //consumer.pause([{ topic }])
       //consumer[socket.id].stop()
 
-      (async () => {
-        await consumer[socket.id].disconnect()
-        consumer[socket.id] = null
-      })();
-      
+      // (async () => {
+      // await consumer[socket.id].disconnect()
+      // consumer[socket.id] = null
+      // })();
+
     }
   });
-
+  socket.on("start", (topic) => {
+    console.info(`Topic resume again : ${topic}]`);
+    consumer.resume([{ topic }]);
+    socket.emit(`status`, topic + " resuming again", "success", true);
+  });
+  socket.on("stop", (topic) => {
+    console.info(`Topic paused : ${topic}]`);
+    consumer.pause([{ topic }]);
+    socket.emit(`status`, topic + " stop resuming", "success", false);
+  });
 });
 
 
 const consume = async (topic, socket) => {
-  consumer[socket.id].run({
+  consumer.run({
     autoCommit: false,
     eachMessage: async ({ topic, partition, message }) => {
       console.log({
+        topic,
         partition,
         offset: message.offset,
         value: message.value.toString(),
       });
-      
+
       socket.emit(`telemetry_topic_message`, message.value.toString());
       //socket.broadcast.to(topic).emit(`telemetry_topic_message`, message.value.toString());
       //socket.to(topic).emit(`telemetry_topic_message`, message.value.toString());
