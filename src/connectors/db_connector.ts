a90bcbe670a5272e28eeaf9f7f07181e2634c370
Murat Alkan <muratalkanw@hotmail.com>
2022-05-08 07:23:26 +0000

    Update db_connector

--- src/connectors/db_connector.ts
+++ src/connectors/db_connector.ts
@@ -5,9 +5,26 @@ dotenv.config();
 // ('prisma');
 // ('sequelize');
 
-export default new Pool({
+
+const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  user: process.env.DB_USER
-});
+  user: process.env.DB_USER,
+  /*ssl: {
+    rejectUnauthorized: false,
+    ca: fs.readFileSync('/path/to/server-certificates/root.crt').toString(),
+    key: fs.readFileSync('/path/to/client-key/postgresql.key').toString(),
+    cert: fs.readFileSync('/path/to/client-certificates/postgresql.crt').toString(),
+  },*/
+})
+
+/*pool
+  .connect()
+  .then(client => {
+    console.log('Connected to DB pool')
+    client.release()
+  })
+  .catch(err => console.error('Error connecting', err.stack))*/
+
+export default pool ;
